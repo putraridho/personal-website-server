492e6d02d1bd8092a2ef0203fe335229e8f0c656
Ridho Putra <muhammadridhoputra@gmail.com>
2023-06-15 17:52:14 +0700

    fix minor stuffs

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,7 @@ async function bootstrap() {
     .setVersion('3.1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    jsonDocumentUrl: 'swagger.json',
-  });
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(1609);
   console.log(`Application is running on: ${await app.getUrl()}`);
